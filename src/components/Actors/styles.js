0b553d175267d3cb7538cbb2f07f12d67d039cc7
Davoud Qaragouzlou <davoodgh0251@gmail.com>
2023-05-26 13:12:01 +0430

    Actors page Completed

---
+++ src/components/Actors/styles.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  image: {
+    maxWidth: '90%',
+    borderRadius: '20px',
+    objectFit: 'cover',
+    boxShadow: '0.5em 0.5em 1em',
+  },
+}))
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -41,9 +41,20 @@ export const tmdbApi = createApi({
     getRecommendations: builder.query({
       query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
     }),
+    getActorsDetails: builder.query({
+      query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+    }),
+    getMoviesByActorId: builder.query({
+      query: (id, page) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
 export const {
-  useGetGenresQuery, useGetMoviesQuery, useGetMovieQuery, useGetRecommendationsQuery,
+  useGetGenresQuery,
+  useGetMoviesQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+  useGetActorsDetailsQuery,
+  useGetMoviesByActorIdQuery,
 } = tmdbApi;
